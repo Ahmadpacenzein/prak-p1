ae1a9a03133dd89b5259f2b83f53b75e6d76c48c
Ahmadpacenzein <ahmadpacen@gmail.com>
2025-05-23 21:03:35 +0700

    Perubahan deskripsi p1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,20 +19,10 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    Ini adalah tampilan dari web saya seorang mahasiswa yang sedang belajar PBK
   </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+  <h5>Silahkan Ikuti Perkembangan saya</h5>
 </template>
 
 <style scoped>
